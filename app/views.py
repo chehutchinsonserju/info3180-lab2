d46c36058a71c0693f09db1601a18dede05533d6
Che <che.hutchinson.serju@gmail.com>
2023-02-12 23:25:42 -0500

    completed profile

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -16,6 +16,14 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile/')
+def profile():
+    """about page"""
+    date_joined = datetime.datetime(1685, 11, 1)
+    d = format_date_joined(date_joined)
+    return render_template('profile.html', date = d)
+
 
 
 ###
@@ -45,3 +53,8 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+
+def format_date_joined(date):
+    return date.strftime("%B, %Y")
